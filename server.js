2d31bba8ab33cd1b757b1f114a3048e15021aca5
spanda <santanu.panda@gcp.lowes.com>
2021-05-24 00:17:15 +0530

    Changes in server.js with port variable

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages')
-const { userJoin, getCurrentUser,userLeaves,getRoomUsers } = require('./utils/users')
+const { userJoin, getCurrentUser,userLeaves } = require('./utils/users')
 
 const app = express();
 const server = http.createServer(app);
@@ -53,7 +53,7 @@ io.on('connection', socket => {
     })
 })
 
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 3000
 
 server.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
 
